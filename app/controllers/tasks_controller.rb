5ccc9498f4f28e2c8d5b069483b63455e91518eb
rikuto yoshida <git.rubyandmarkup@gmail.com>
2018-09-12 08:40:40 +0000

    CRUD messages

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,23 @@
+class TasksController < ApplicationController
+  def create
+  end 
+  
+  def index
+     @tasks=Task.all
+  end 
+  
+  def show
+  end
+   
+  def destroy
+  end
+  
+  def edit
+  end
+  
+  def new
+  end
+  
+  def update
+  end 
+end
