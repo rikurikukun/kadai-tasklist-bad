807fc86675150487783561e059255ea1f9b29339
rikuto yoshida <git.rubyandmarkup@gmail.com>
2018-09-12 09:32:00 +0000

    last

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,14 @@
 class TasksController < ApplicationController
   def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      flash[:success] = 'Taskが正常に投稿されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task が投稿されませんでした'
+      render :new
+    end 
   end 
   
   def index
@@ -11,14 +20,30 @@ class TasksController < ApplicationController
   end
    
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
   end
   
   def edit
+     @message = Message.find(params[:id])
   end
   
   def new
+     @task = Task.new
   end
   
   def update
-  end 
+  end
+
+ private
+
+  
+  def task_params
+    params.require(:task).permit(:content)
+  end
+
 end
+
